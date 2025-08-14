88c866790b374a69c17c5dd8de5da66165d2ba3d
LasseVKP <Lassevkp@gmail.com>
2025-08-14 14:03:42 +0200

    Make a broken conway's game of life

--- build.bat
+++ build.bat
@@ -1,1 +1,3 @@
-gcc main.c -o main.exe -I C:\raylib\include -L C:\raylib\lib -lraylib -lopengl32 -lgdi32 -lwinmm
+del conway.exe
+gcc main.c -o conway.exe -I C:\raylib\include -L C:\raylib\lib -lraylib -lopengl32 -lgdi32 -lwinmm
+conway.exe
--- main.c
+++ main.c
@@ -1,15 +1,92 @@
+// Use raylib and raygui for graphics
 #include "raylib.h"
 #define RAYGUI_IMPLEMENTATION
 #include "raygui.h"
+#include "stdlib.h"
+
+#define BOARD_SIZE 50
+#define WINDOW_SIZE 1000
+#define CELL_SIZE (WINDOW_SIZE/BOARD_SIZE)
+
+bool board[BOARD_SIZE][BOARD_SIZE];
+bool previousBoard[BOARD_SIZE][BOARD_SIZE];
+
+void initBoard() {
+    /*for (int x = 0; x < BOARD_SIZE; x++) {
+        for (int y = 0; y < BOARD_SIZE; y++) {
+            board[x][y] = rand() % 2 - 1;
+        }
+    }*/
+
+   /* Coordinates for a flying thingy*/
+   board[5+5][5] = true;
+   board[6+5][5] = true;
+   board[7+5][5] = true;
+   board[7+5][4] = true;
+   board[6+5][3] = true;
+   
+}
+
+void updateBoard() {
+    // Copy to a temporary board
+    memcpy(previousBoard, board, sizeof(board));
+    for (size_t x = 0; x < BOARD_SIZE; x++)
+    {
+        for (size_t y = 0; y < BOARD_SIZE; y++)
+        {
+            bool state = previousBoard[x][y];
+
+            // Get amount of alive neighbours
+            int neighbours = 0;
+            neighbours += previousBoard[((x+1)<BOARD_SIZE) ? x+1 : 0][y];
+            neighbours += previousBoard[((x+1)<BOARD_SIZE) ? x+1 : 0][((y+1)<BOARD_SIZE) ? y+1 : 0];
+            neighbours += previousBoard[((x+1)<BOARD_SIZE) ? x+1 : 0][((y-1)>=0) ? y-1 : BOARD_SIZE];
+
+            neighbours += previousBoard[((x-1)>=0) ? x-1 : BOARD_SIZE][y];
+            neighbours += previousBoard[((x-1)>=0) ? x-1 : BOARD_SIZE][((y+1)<BOARD_SIZE) ? y+1 : 0];
+            neighbours += previousBoard[((x-1)>=0) ? x-1 : BOARD_SIZE][((y-1)>=0) ? y-1 : BOARD_SIZE];
+
+            neighbours += previousBoard[x][((y+1)<BOARD_SIZE) ? y+1 : 0];
+            neighbours += previousBoard[x][((y-1)>=0) ? y-1 : BOARD_SIZE];
+
+            if(!state && neighbours == 3) {
+                board[x][y] = true;
+            } else if(state && (neighbours < 2 || neighbours > 3)) {
+                board[x][y] = false;
+            }
+        }
+    }
+    
+
+}
+
+void drawBoard(){
+    for (size_t x = 0; x < BOARD_SIZE; x++)
+    {
+        for (size_t y = 0; y < BOARD_SIZE; y++)
+        {
+            if(board[x][y] == true){
+                DrawRectangle(x*CELL_SIZE, y*CELL_SIZE, CELL_SIZE, CELL_SIZE, BLACK);
+            }
+        }
+    }
+}
 
 int main(void) {
-    InitWindow(800, 450, "raylib + raygui");
+    // Open the window
+    InitWindow(WINDOW_SIZE, WINDOW_SIZE, "Conway's Game Of Life");
+    SetTargetFPS(20);
+
+    // Set start pattern
+    initBoard();
+
+    // Main loop
     while (!WindowShouldClose()) {
+        // Clear and update
         BeginDrawing();
         ClearBackground(RAYWHITE);
-        if (GuiButton((Rectangle){ 350, 200, 100, 40 }, "Click Me")) {
-            // Do something
-        }
+        updateBoard();
+        drawBoard();
         EndDrawing();
     }
     CloseWindow();
