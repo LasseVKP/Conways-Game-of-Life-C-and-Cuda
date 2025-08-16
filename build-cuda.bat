0835a2dc1a8fd8ff2b651729c10f368900795712
LasseVKP <Lassevkp@gmail.com>
2025-08-16 02:16:42 +0200

    Remove windows build scripts

--- build-cuda.bat
+++
@@ -1,3 +0,0 @@
-del conway.exe
-nvcc main.c -o conway.exe -I /usr/local/cuda/include -I C:\raylib\include -L C:\raylib\lib -lraylib -lopengl32 -lgdi32 -lwinmm
-conway.exe
--- build.bat
+++
@@ -1,3 +0,0 @@
-del conway.exe
-gcc main.c -o conway.exe -I C:\raylib\include -L C:\raylib\lib -lraylib -lopengl32 -lgdi32 -lwinmm
-conway.exe
