a3abcd9ecc27904801724531436b28b194e54902
LasseVKP <Lassevkp@gmail.com>
2025-08-17 18:26:02 +0200

    Remove unnecessary files

--- build-cuda.sh
+++
@@ -1,3 +0,0 @@
-rm conwayCuda
-/usr/local/cuda/bin/nvcc main.cu -o conwayCuda -I /usr/local/cuda/include -I include -L lib -lraylib -lGL -lm -lpthread -ldl -lrt -lX11
-./conwayCuda
--- build.sh
+++
@@ -1,3 +0,0 @@
-rm conway
-cc -o conway main.c -I include -L lib -lraylib -lGL -lm -lpthread -ldl -lrt -lX11
-./conway
